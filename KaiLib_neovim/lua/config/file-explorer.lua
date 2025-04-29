ae09721595b8d44b3b486a43340b96dd49809d74
Zekai Liang <19465873+g20090279@users.noreply.github.com>
2025-04-29 23:24:12 +0200

    update root for nvim tree, set wrap

--- KaiLib_neovim/lua/config/file-explorer.lua
+++ KaiLib_neovim/lua/config/file-explorer.lua
@@ -19,5 +19,9 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = true,
     },
+    update_focused_file = {
+        enable = true,
+        update_root = true,  -- this makes the tree follow the buffer's directory
+    },
 })
 
