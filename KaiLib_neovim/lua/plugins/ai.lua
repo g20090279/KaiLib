5aa406fa741203052ed7e2890d34d2f13c01d366
Zekai Liang <19465873+g20090279@users.noreply.github.com>
2025-07-14 23:12:19 +0200

    use copilot as first ai assistant

--- KaiLib_neovim/lua/plugins/ai.lua
+++ KaiLib_neovim/lua/plugins/ai.lua
@@ -1,7 +1,13 @@
 return {
-    --{ "github/copilot.vim" },
-    {
-      'kiddos/gemini.nvim',
-      opts = {},
-    },
+    -- Copilot
+    { "github/copilot.vim" },
+
+    -- Gemini
+    -- Uncomment the lines below to enable Gemini support.
+    -- Note: Ensure you have the necessary setup for Gemini.
+    -- {
+    --   'kiddos/gemini.nvim',
+    --   dependencies = { 'nvim-lua/plenary.nvim' },
+    --   opts = {},
+    --  },
 }
