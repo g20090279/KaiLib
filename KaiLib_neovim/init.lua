a1527deeb7d8eabe80f6ddaacb253fe53084c28b
Zekai Liang <19465873+g20090279@users.noreply.github.com>
2025-08-26 15:34:03 +0200

    update theme

--- KaiLib_neovim/init.lua
+++ KaiLib_neovim/init.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_user_command("CdRoot", function()
+    local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+    if git_root and git_root ~= "" then
+        vim.cmd("cd " .. git_root)
+        print("Changed dir to " .. git_root)
+    else
+        print("Not inside a Git repo")
+    end
+end, {})
+
 -- require("config.ai")
 require("config.options")
 require("config.lazy")
@@ -8,3 +18,4 @@ require("config.cmp")
 require("config.file-explorer")
 require("config.statusline")
 require("config.debugger")
+
--- KaiLib_neovim/lua/plugins/theme.lua
+++ KaiLib_neovim/lua/plugins/theme.lua
@@ -5,28 +5,56 @@ return {
         branch = 'master',
         lazy = false,
     },
+    -- {
+    --     "folke/snacks.nvim",
+    --     priority = 1000,
+    --     lazy = false,
+    --     ---@type snacks.Config
+    --     opts = {
+    --         -- your configuration comes here
+    --         -- or leave it empty to use the default settings
+    --         -- refer to the configuration section below
+    --         bigfile = { enabled = true },
+    --         explorer = { enabled = true },
+    --         indent = { enabled = true },
+    --         input = { enabled = true },
+    --         picker = { enabled = true },
+    --         notifier = { enabled = true },
+    --         quickfile = { enabled = true },
+    --         scope = { enabled = true },
+    --         scroll = { enabled = true },
+    --         statuscolumn = { enabled = true },
+    --         words = { enabled = true },
+    --     },
+    -- },
     {
-        "folke/snacks.nvim",
-        priority = 1000,
-        lazy = false,
-        ---@type snacks.Config
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-            bigfile = { enabled = true },
-            explorer = { enabled = true },
-            indent = { enabled = true },
-            input = { enabled = true },
-            picker = { enabled = true },
-            notifier = { enabled = true },
-            quickfile = { enabled = true },
-            scope = { enabled = true },
-            scroll = { enabled = true },
-            statuscolumn = { enabled = true },
-            words = { enabled = true },
-        },
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
     },
     { "MunifTanjim/nui.nvim" },
     { "mhinz/vim-startify" },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+    },
+    { "rcarriga/nvim-notify" },
+    { "RRethy/vim-illuminate" },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
 }
