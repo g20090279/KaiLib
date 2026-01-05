07c75fc134eed391d4d06c629cec8178fd6d5b5d
Zekai Liang <19465873+g20090279@users.noreply.github.com>
2026-01-05 20:43:09 +0800

    delete nvim settings

--- KaiLib_neovim/lua/plugins/file-explorer.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {
-        "preservim/nerdtree",
-        cmd = { "NERDTreeToggle", "NERDTreeFind" },
-        dependencies = { "Xuyuanp/nerdtree-git-plugin", "ryanoasis/vim-devicons" },  -- optional for icon support
-    },
-}
--- KaiLib_neovim/lua/plugins/lsp.lua
+++
@@ -1,6 +0,0 @@
-return {
-    { "williamboman/mason.nvim", config = true },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-}
